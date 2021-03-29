f25abb13c06d266955798392f771eaf915d0a18d
Yuri Dias Soares <yurids@pop-os.localdomain>
2021-03-29 14:52:27 -0300

    att

--- ecommerce/src/main/java/com/desafioEcommerce/Ecommerce/Models/Usuario.java
+++ ecommerce/src/main/java/com/desafioEcommerce/Ecommerce/Models/Usuario.java
@@ -2,6 +2,7 @@
 package com.desafioEcommerce.Ecommerce.Models;
 
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
--- ecommerce/src/main/java/com/desafioEcommerce/Ecommerce/Service/UsuarioService.java
+++ ecommerce/src/main/java/com/desafioEcommerce/Ecommerce/Service/UsuarioService.java
@@ -1,5 +1,8 @@
 package com.desafioEcommerce.Ecommerce.Service;
 
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +17,24 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repositoryUsuario;
 	
-	
-	
+	public ResponseEntity<List<Usuario>>findAllUsuario(){
+		return ResponseEntity.ok(repositoryUsuario.findAll());
+	}
+	public ResponseEntity<Usuario> findAllByIdUsuario(Long id){
+		return repositoryUsuario.findById(id)
+				.map(usu -> ResponseEntity.ok(usu))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	public ResponseEntity<List<Usuario>>findAllUsuarioByNome(String name){
+		return ResponseEntity.ok(repositoryUsuario.findAllUsuario(name));
+	}
 	public ResponseEntity<Usuario>postNewUsuario(Usuario usuario){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repositoryUsuario.save(usuario));
 	}
-	
+	public ResponseEntity<Usuario>putEditUsuario(Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositoryUsuario.save(usuario));
+	}
+	public void deleteUsuario(Long id) {
+		repositoryUsuario.deleteById(id);
+	}
 }
