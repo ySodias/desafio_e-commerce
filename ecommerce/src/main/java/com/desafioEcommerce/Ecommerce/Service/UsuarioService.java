38cd8b5cc56d86a9c5dc5bc4501996acc448faa9
Yuri Dias Soares <yurids@pop-os.localdomain>
2021-03-29 13:32:56 -0300

    adicionando query

--- ecommerce/src/main/java/com/desafioEcommerce/Ecommerce/Service/UsuarioService.java
+++ ecommerce/src/main/java/com/desafioEcommerce/Ecommerce/Service/UsuarioService.java
@@ -1,4 +1,23 @@
 package com.desafioEcommerce.Ecommerce.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.desafioEcommerce.Ecommerce.Models.Usuario;
+import com.desafioEcommerce.Ecommerce.Repository.UsuarioRepository;
+
+@Service
 public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository repositoryUsuario;
+	
+	
+	
+	public ResponseEntity<Usuario>postNewUsuario(Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositoryUsuario.save(usuario));
+	}
+	
 }
